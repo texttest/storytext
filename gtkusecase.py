10380c2f96427b634cd7bd1af66c318417ee4a2f
geoff <>
2006-10-09 16:23:27 +0000

    unselect_iter also changes selections

--- gtkusecase.py
+++ gtkusecase.py
@@ -290,7 +290,7 @@ class TreeSelectionEvent(StateChangeEvent):
         else:
             return model
     def getProgrammaticChangeMethods(self):
-        return [ self.widget.unselect_all, self.widget.select_iter, \
+        return [ self.widget.unselect_all, self.widget.select_iter, self.widget.unselect_iter, \
                  self.widget.get_tree_view().row_activated, self.widget.get_tree_view().collapse_row, \
                  self.getModel().remove ]
     def getStateDescription(self):
