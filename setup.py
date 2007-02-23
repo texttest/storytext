a4763a43d68a1680a816ae147b8f1679860749b4
geoff <>
2007-02-23 11:49:19 +0000

    Buggy Imake installation replaced with Python standard setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='PyUseCase',
+      version='1.2.devel',
+      py_modules=["usecase", "gtkusecase", "paths", "log4py", "process"],
+      )
