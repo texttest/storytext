bcc77ee7ca694569927dcabe935e723514b224d0
Geoff Bache <geoff.bache@jeppesen.com>
2010-01-18 15:29:49 +0100

    Making it work on Windows again

--- lib/setup.py
+++ lib/setup.py
@@ -13,7 +13,8 @@ mod_files = [ f[:-3] for f in glob("*.py") ]
 mod_files.remove("setup")
 if os.name == "nt":
     make_windows_script("../bin/pyusecase")
-    scripts=["../bin/pyusecase.py", "../bin/pyusecase.exe"]
+    make_windows_script("../bin/usecase_name_chooser")
+    scripts=["../bin/pyusecase.py", "../bin/pyusecase.exe", "../bin/usecase_name_chooser.py", "../bin/usecase_name_chooser.exe"]
 else:
     scripts=["../bin/pyusecase","../bin/usecase_name_chooser"]
 
