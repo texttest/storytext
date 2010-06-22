db7e57699ace42c9c5acd0d1bfb311d5f5ba5ff5
Geoff Bache <geoff.bache@jeppesen.com>
2010-06-22 14:55:51 +0200

    Upgrading requirements to Python 2.6

--- lib/guiusecase.py
+++ lib/guiusecase.py
@@ -4,12 +4,7 @@ stuff also applicable even without this """
 
 import usecase, os, sys, logging, subprocess
 from ndict import seqdict
-
-# We really need our ConfigParser to be ordered, copied the one from 2.6 into the repository
-if sys.version_info[:2] >= (2, 6):
-    from ConfigParser import ConfigParser
-else:
-    from ConfigParser26 import ConfigParser
+from ConfigParser import ConfigParser
 
 class GuiEvent(usecase.UserEvent):
     def __init__(self, name, widget, *args):
