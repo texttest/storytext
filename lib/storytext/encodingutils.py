4c3ac5c627471292013a91a641ef4e6c97a10852
Geoff Bache <geoff.bache@jeppesen.com>
2013-03-01 18:01:19 +0100

    Cache the locale, can lead to deadlock if 2 threads read it at once

--- lib/storytext/encodingutils.py
+++ lib/storytext/encodingutils.py
@@ -1,23 +1,17 @@
 
 import sys, codecs, logging, locale
 
+localeEncoding = None
+
 def openEncoded(file, mode="r"):
-    try:
-        encoding = getLocaleEncoding()
-        return codecs.open(file, mode, encoding, errors="replace")
-    except ValueError:
-        return open(file, mode)
+    encoding = getLocaleEncoding()
+    return codecs.open(file, mode, encoding, errors="replace")
     
 def encodeToLocale(unicodeText):
     if pythonVersion3():
         return unicodeText # don't need to mess about if we're in Python 3 anyway
     elif unicodeText:
-        try:
-            return unicodeText.encode(getLocaleEncoding(), 'replace')
-        except ValueError:
-            # Get this if locale is invalid for example
-            # Return the text as-is and hope for the best
-            return unicodeText
+        return unicodeText.encode(getLocaleEncoding(), 'replace')
     else:
         return ""
         
@@ -25,8 +19,17 @@ def pythonVersion3():
     return sys.version_info[0] == 3
     
 def getLocaleEncoding():
-    return locale.getdefaultlocale()[1] or "utf-8"
-
+    global localeEncoding
+    if localeEncoding:
+        return localeEncoding
+    
+    try:
+        localeEncoding = locale.getdefaultlocale()[1] or "utf-8"
+    except ValueError:
+        # Get this if locale is invalid for example
+        # Return the text as-is and hope for the best
+        localeEncoding = "utf-8"
+    return localeEncoding
     
 class EncodingLoggerProxy:
     def __init__(self, logger):
