40f249adb51ccaefd0243f8d796277b6f3ab9e78
Daniel Galdames <daniel.galdames@jeppesen.com>
2013-04-26 15:42:25 +0200

    TTT-3124: eliminate race conditions from jobs finishing simultaneously, don't duplicate the events

--- lib/storytext/javarcptoolkit/jobsynchroniser.py
+++ lib/storytext/javarcptoolkit/jobsynchroniser.py
@@ -16,50 +16,51 @@ class JobListener(JobChangeAdapter):
         self.jobNameToUse = None
         self.jobCount = 0
         self.jobCountLock = Lock()
-        self.afterOthers = False
         self.logger = logging.getLogger("Eclipse RCP jobs")
         
     def done(self, e):
         storytext.guishared.catchAll(self.jobDone, e)
         
     def jobDone(self, e):
-        jobName = e.getJob().getName().lower()        
-        self.alterJobCount(-1)
+        jobName = e.getJob().getName().lower()
+        self.jobCountLock.acquire()
+        self.jobCount -= 1
         self.logger.debug("Completed " + ("system" if e.getJob().isSystem() else "non-system") + " job '" + jobName + "' jobs = " + repr(self.jobCount))    
         # We wait for the system to reach a stable state, i.e. no scheduled jobs
         # Would be nice to call Job.getJobManager().isIdle(),
         # but that doesn't count scheduled jobs for some reason
-        if self.jobCount == 0 and self.jobNameToUse: 
+        noScheduledJobs = self.jobCount == 0
+        self.jobCountLock.release()        
+        if noScheduledJobs and self.jobNameToUse: 
             self.setComplete()
 
     def setComplete(self):
         DisplayFilter.registerApplicationEvent("completion of " + self.jobNameToUse, category="jobs")
         self.jobNameToUse = None
 
-    def alterJobCount(self, value):
-        self.jobCountLock.acquire()
-        self.jobCount += value
-        self.jobCountLock.release()
-
     def scheduled(self, e):
         storytext.guishared.catchAll(self.jobScheduled, e)
         
     def jobScheduled(self, e):
         jobName = e.getJob().getName().lower()
-        self.alterJobCount(1)
+        self.jobCountLock.acquire()
+        self.jobCount += 1
         self.logger.debug("Scheduled job '" + jobName + "' jobs = " + repr(self.jobCount))
+        self.jobCountLock.release()
         if jobName in self.systemJobNames or not e.getJob().isSystem():
             self.logger.debug("Now using job name '" + jobName + "'")
             self.jobNameToUse = jobName
         
         # As soon as we can, we move to the back of the list, so that jobs scheduled in 'done' methods get noticed
         if not e.getJob().isSystem():
-            self.afterOthers = True
+            self.jobCountLock.acquire()
             Job.getJobManager().removeJobChangeListener(self)
             Job.getJobManager().addJobChangeListener(self)
+            self.jobCountLock.release()
             self.logger.debug("At back of list now")
-        
+            
     @classmethod
     def enable(cls, *args):
         cls.instance = cls(*args)
+        cls.instance.logger.debug("Enabling Job Change Listener")
         Job.getJobManager().addJobChangeListener(cls.instance)
